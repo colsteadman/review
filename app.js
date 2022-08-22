1deec5db5edf66b4d94efb6e6b223fee7cf452a1
Collin Steadman <907812@H2WHK1B8Q6NW.tld>
2022-08-22 08:09:24 -0500

    just need to fix errors then im done with js 1

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ function getData(){
     var adj = document.getElementById("adj").value;
     var adverb = document.getElementById("adverb").value;
   
-    return [place, person, verb, adj, adverb]
+    document.getElementById("sentence").innerHTML = sentences(verb, adverb, person, place, adj)
+    
 }
 
-function sentences(){
+function sentences(verb, adverb, person, place, adj){
     sentenceArray = [`Today I had to ${verb} ${adverb}. But right in the middle of it, ${person} came right in the middle with this ${adj} book and wanted to go to ${place}`, `${person} was in ${place} trying to ${verb}. However, ${person} left a ${adj} shirt behind, while crying ${adverb}.`, `its been a long time since ${person} decided to ${verb} in ${place}. What some people said was that ${person} decided to ${verb} in ${place}. What some people said was that ${person} screemed "This strawberry is ${adj}!! we thought ${person} wnet crazy!`]
-ranN = Math.floor(Math.random() * sentencesArray.length)
+ranN = Math.floor(Math.random() * sentenceArray.length)
 
 return sentenceArray(ranN)
 }
